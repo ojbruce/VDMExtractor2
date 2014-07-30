709f6102d48cab0a99d2a14b1e19c4627561f3a7
ojbruce <olivia.j.bruce@gmail.com>
2014-07-30 14:08:18 +0200

    Created extractor first service

---
+++ src/VDMExtractor/ExtractorBundle/Entity/Post.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace VDMExtractor\ExtractorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use \DateTime;
+
+/**
+ * Post Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="post")
+ */
+class Post
+{
+	/**
+	 * @var int $id Identifier
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 */
+	protected $id;
+
+	/**
+	 * @var string $author Author
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $author;
+
+	/**
+	 * @var DateTime $date Date
+	 * @ORM\Column(type="datetime")
+	 */
+	protected $date;
+
+	/**
+	 * @var string $content
+	 * @ORM\Column(type="string")
+	 */
+	protected $content;
+
+	public function __construct($id, $author, DateTime $date, $content)
+	{
+		$this->id      = $id;
+		$this->author  = $author;
+		$this->date    = $date;
+		$this->content = $content;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	public function getAuthor()
+	{
+		return $this->author;
+	}
+
+	public function setAuthor($author)
+	{
+		$this->author = $author;
+		return $this;
+	}
+
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+	public function setDate(DateTime $date)
+	{
+		$this->date = $date;
+		return $this;
+	}
+
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	public function setContent($content)
+	{
+		$this->content = $content;
+		return $this;
+	}
+
+	public function toArray()
+	{
+		return [
+			'id'      => $this->id,
+			'content' => utf8_encode($this->content),
+			'date'    => $this->date->format('Y-m-d H:m:s'),
+			'author'  => $this->author,
+		]; 
+	}
+}
+
