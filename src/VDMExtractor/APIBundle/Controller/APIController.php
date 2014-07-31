b8288c7e0b5608aacf5bbac1b10fc41a6b140f5a
ojbruce <olivia.j.bruce@gmail.com>
2014-08-01 01:53:53 +0200

    Fix controller and routing

--- src/VDMExtractor/APIBundle/Controller/APIController.php
+++ src/VDMExtractor/APIBundle/Controller/APIController.php
@@ -3,6 +3,10 @@
 namespace VDMExtractor\APIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+use VDMExtractor\ExtractorBundle\Entity;
 
 /**
  * API's controller
@@ -15,13 +19,20 @@ class APIController extends Controller
         return $this->render('VDMExtractorAPIBundle:Default:index.html.twig', array('name' => $name));
     }
 
-    
-    public function getUniqueAction()
+    /**
+     * Action that will render a unique post
+     * Using doctrine to retrieve
+     */
+    public function getUniqueAction($id)
     {
-        $id = (int) $this->params()->fromRoute('id', null);
 
-        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
-        $post          = $entityManager->find('Extractor\Entity\Post', $id);
+        /** Getting doctrine entity */
+        $repository = $this->getDoctrine()
+                           ->getManager()
+                           ->getRepository('VDMExtractorExtractorBundle:Post');
+
+        /** Getting back a specific */                   
+        $post = $repository->findOneById($id);
 
         if ($post) {
             $result[] = $post->toArray();
@@ -29,22 +40,30 @@ class APIController extends Controller
             $result   = ['success' => 'false']; 
         }
 
-        return new JsonModel([
-            'post'  => $result
-        ]);
+        return new Response(json_encode(array([
+            'post' => $result
+        ])));
     }
 
+    /**
+     * Action that will render stocked posts
+     * Using doctrine to retrieve
+     */
     public function getAction()
     {
-        // Collect GET params
-        $to     = $this->params()->fromQuery('to', null);
-        $from   = $this->params()->fromQuery('from', null);
-        $author = $this->params()->fromQuery('author', null);
+        // We retrieve each querystrings
+        $request = $this->getRequest();
+        $to      = $request->query->get('to');
+        $from    = $request->query->get('from');
+        $author  = $request->query->get('author');
+        
+        // Getting doctrine entity 
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('VDMExtractorExtractorBundle:Post');
 
-        $entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
 
         // Create query with filters or not
-        $queryDQL = 'SELECT p FROM Extractor\Entity\Post p WHERE ';
+        $queryDQL = 'SELECT p FROM VDMExtractorExtractorBundle:Post p WHERE ';
 
         // Author filter
         if ($author) {
@@ -60,8 +79,12 @@ class APIController extends Controller
             }
         }
 
-        $queryDQL .= '1 = 1';
-        $query     = $entityManager->createQuery($queryDQL);
+        $queryDQL .= '1=1';
+
+        // Creating query
+        $query = $em->createQuery(
+            $queryDQL
+        );
 
         // Set query parameters
         if ($author) {
@@ -83,10 +106,10 @@ class APIController extends Controller
             $postsJsonified[] = $post->toArray();
         }
 
-        return new JsonModel([
+        return new Response(json_encode([
             'posts'  => $postsJsonified,
             'count' => count($postsJsonified),
-        ]);
+        ]));
     }
 
 }
